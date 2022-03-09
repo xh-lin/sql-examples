6a922a50e18532949107863ab5fde84970f8e2f6
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-03-09 11:48:02 -0500

    User model POJO

---
+++ hibernate/src/main/java/com/perscholas/hibernate_demo/model/User.java
@@ -0,0 +1,81 @@
+package com.perscholas.hibernate_demo.model;
+
+public class User {
+
+    private Integer id;
+    private String fullname;
+    private String email;
+    private String password;
+    private int age;
+    private double salary;
+    private String city;
+
+    public User() { }
+
+    public User(Integer id, String fullname, String email, String password, int age, double salary, String city) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.salary = salary;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return this.fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
